ded9ad2e530967c0b3a8ceaba3cd873240b93785
Vjacheslav Bezborodov <waww@live.ru>
2015-06-09 11:56:01 +0500

    Add make.sh

---
+++ make.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Writing files to \"~/Desktop/DiplomaPrint\"..."
+
+mkdir -p ~/Desktop/DiplomaPrint
+
+cp ~/Documents/Diploma/Theory/Paper/Bezborodov_Diploma_Parallel_implementation_of_the_ellipsoids_method_for_optimization_problems_of_large_dimension.pdf ~/Desktop/DiplomaPrint/Paper.pdf
+cp ~/Documents/Diploma/Theory/Presentation/Handout/Bezborodov_Handout_Parallel_implementation_of_the_ellipsoids_method_for_optimization_problems_of_large_dimension.pdf ~/Desktop/DiplomaPrint/Handout.pdf
+cp ~/Documents/Diploma/Theory/Presentation/Slides/Bezborodov_Presentation_Parallel_implementation_of_the_ellipsoids_method_for_optimization_problems_of_large_dimension.pdf ~/Desktop/DiplomaPrint/Bezborodov_Presentation.pdf
+cp ~/Documents/Diploma/Theory/Presentation/Thesis/Bezborodov_Thesis_Parallel_implementation_of_the_ellipsoids_method_for_optimization_problems_of_large_dimension.pdf ~/Desktop/DiplomaPrint/Thesis.pdf
+
+echo "Done"
